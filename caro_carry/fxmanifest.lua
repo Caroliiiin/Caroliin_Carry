3e4432e836f047b209d31ba70be3bc4e7b31fc14
Carolin <40367513+Caroliiiin@users.noreply.github.com>
2024-02-05 23:58:50 +0100

    Add files via upload

---
+++ caro_carry/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Caroliin'
+description 'Updated Version of CarryPeople'
+version '1.0.0'
+
+client_script "client.lua"
+server_script "server.lua"
